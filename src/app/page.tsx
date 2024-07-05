558a393f7a8d9b1894f74dbdd1d4e52cce61fd63
Samuel Ayegbusi <Stemitope370@gmail.com>
2024-07-05 17:07:59 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="bg-black-100 items-center text-white-100 py-10 px-20 flex justify-center">
+      Hello world
+    </main>
+  );
+}
---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,42 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const badgeVariants = cva(
+  "inline-flex items-center rounded-sm capitalize border px-1.5 h-8 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default:
+          "border-transparent  bg-blue text-blue-foreground hover:bg-blue/80",
+        secondary:
+          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        destructive:
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        outline: "text-foreground",
+        red: "border-transparent bg-red-light text-red-100 hover:bg-red/80",
+        blue: "border-transparent bg-blue-light text-blue-100 hover:bg-blue/80",
+        purple:
+          "border-transparent bg-purple-light text-purple-100 hover:bg-purple/80",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, children, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props}>
+      {children}{" "}
+    </div>
+  );
+}
+
+export { Badge, badgeVariants };
